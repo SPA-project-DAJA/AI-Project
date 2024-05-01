7ddc70176f51473f61ab67bcb5c362ad9f984187
dadagg53 <danieljoskowski6@wp.pl>
2024-05-01 17:34:50 +0200

    TPodłaczone api Pexels, dodana galeria zdjec, dodane wyszkiwanie przez wcisniecie ENTER

--- components/Top/Nav.vue
+++ components/Top/Nav.vue
@@ -1,72 +1,54 @@
-<script setup>
-
-</script>
 <template>
- <nav class="navbar" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <a class="navbar-item" href="https://bulma.io">
-      <img src="https://img.freepik.com/darmowe-wektory/ptak-kolorowe-logo-wektor-gradientu_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.632798143.1713571200&semt=ais" height="112">
-    </a>
-
-    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
-
-  <div id="navbarBasicExample" class="navbar-menu">
-    <div class="navbar-start">
-      <NuxtLink to="/" class="navbar-item">
-        Home
-      </NuxtLink>
-
-      <NuxtLink to="/about" class="navbar-item">
-        about
-      </NuxtLink>
-
-      <a href="/about" class="navbar-item">
-        about (nie spa)
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="#">
+        <img
+          src="https://img.freepik.com/darmowe-wektory/ptak-kolorowe-logo-wektor-gradientu_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.632798143.1713571200&semt=ais"
+          alt="Logo"
+        />
       </a>
 
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link">
-          More
-        </a>
+      <a
+        role="button"
+        class="navbar-burger burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
 
-        <div class="navbar-dropdown">
-          <a class="navbar-item">
-            About
-          </a>
-          <a class="navbar-item">
-            Jobs
-          </a>
-          <a class="navbar-item">
-            Contact
-          </a>
-          <hr class="navbar-divider">
-          <a class="navbar-item">
-            Report an issue
-          </a>
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item" href="#">Home</a>
+        <a class="navbar-item" href="#">About</a>
+        <a class="navbar-item" href="#">Services</a>
+        <a class="navbar-item" href="#">Contact</a>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Sign up</strong>
+            </a>
+            <a class="button is-light">Log in</a>
+          </div>
         </div>
       </div>
     </div>
-
-    <div class="navbar-end">
-      <div class="navbar-item">
-        <div class="buttons">
-          <a class="button is-primary">
-            <strong>Sign up</strong>
-          </a>
-          <a class="button is-light">
-            Log in
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
-</nav>
+  </nav>
 </template>
-<style scoped lang="scss">
 
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scoped>
+/* Dodaj tutaj niestandardowe style */
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,54 +1,129 @@
+<template>
+  <div>
+    <input
+      type="text"
+      v-model="searchQuery"
+      placeholder="Wyszukaj..."
+      @keydown.enter="searchData"
+    />
+    <button @click="searchData" :disabled="loader">Wyszukaj</button>
+    <div v-if="loader" class="loader">Ładowanie danych...</div>
+
+    <div class="result" v-else>
+      <div v-for="(photo, index) in photos" :key="index" class="result-item">
+        <img
+          id="thumbnail"
+          :src="photo.src.medium"
+          :alt="photo.photographer"
+          @click="showFullImage(photo.src.original)"
+        />
+        <div class="modal" v-if="selectedImage === photo.src.original">
+          <span @click="selectedImage = ''" class="close">✕</span>
+          <img id="big" :src="selectedImage" :alt="photo.photographer" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
 <script setup>
-const searchQuery = ref("star")
-const resultQuery = ref(null)
-const arrayData = ref(null)
-const loader = ref(false)
-const searchData = async() => {
-    loader.value = true
-    await useFetch('https://images-api.nasa.gov/search?q=' + searchQuery.value).then((response) => {
-        if(response.data) {
-            setTimeout(() => {
-                resultQuery.value = response.data.value.collection.items
-                loader.value = false
-                console.log(response.data.value.collection)
-            }, 10000)
-        }
-    }) 
+import { ref } from "vue";
+
+const searchQuery = ref("");
+const photos = ref([]);
+const loader = ref(false);
+const selectedImage = ref("");
+
+const API_KEY = "s4lVXRGh8RuRzi7fWhTYeNQHyZfuqSIV2LpZNlEkecIBVFcMXbW03gKE";
+const BASE_URL = "https://api.pexels.com/v1";
+
+const searchData = async () => {
+  if (!searchQuery.value) return;
+  loader.value = true;
+
+  try {
+    const response = await fetch(
+      `${BASE_URL}/search?query=${searchQuery.value}&per_page=15`,
+      {
+        headers: {
+          Authorization: API_KEY,
+        },
+      }
+    );
+    const data = await response.json();
+    if (data.photos) {
+      photos.value = data.photos;
+      loader.value = false;
+      console.log(data); // Wyświetla dane w konsoli
+    } else {
+      console.error("Nie znaleziono zdjęć");
+    }
+  } catch (error) {
+    console.error("Błąd pobierania danych:", error);
+    loader.value = false;
+  }
+};
+
+const showFullImage = (imageUrl) => {
+  selectedImage.value = imageUrl;
+};
+</script>
+
+<style scoped>
+.loader {
+  margin-top: 20px;
 }
 
-onBeforeMount(async () => {
-    await searchData()
-});
+.result {
+  display: flex;
+  flex-wrap: wrap;
+}
 
-// Użycie watch do obserwacji resultQuery
-watch(resultQuery, (newValue, oldValue) => {
-    arrayData.value = newValue
+.result-item {
+  margin-right: 10px;
+  margin-bottom: 10px;
+}
 
-    console.log(newValue); // Wyświetla nową wartość resultQuery.value
-    // Możesz tutaj dodać dodatkowe logikę, która ma się wykonać po zmianie wartości
-}, {
-    deep: true // opcja 'deep' jest przydatna, jeśli obserwowany obiekt jest złożony (np. obiekt lub tablica)
-});
-</script>
-<template>
-    <div>
-        <input type="text" v-model="searchQuery">
-        <button @click="searchData" v-if="!loader">Wyszukaj</button>
-        <button @click="searchData" disabled v-else>Wyszukaj</button>
-        <div class="cos" v-if="loader">To jest ladowanie danych</div>
+.thumbnail img {
+  width: 200px;
+  height: auto;
+  cursor: pointer;
+}
 
-        <div class="result" v-else>
-            <div v-for="(value, index) in arrayData" :key=index>
-                {{  value.links }}
-        </div>
-        </div>
-        
-    </div>
-</template>
-<style scoped lang="scss">
-h3 {
-    span {
-        color: green
-    }
+.modal {
+  display: block;
+  position: fixed;
+  z-index: 1;
+  padding-top: 100px;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.modal img {
+  margin: auto;
+  display: block;
+  max-width: 90%;
+  max-height: 90%;
+}
+
+.close {
+  color: white;
+  position: absolute;
+  top: 15px;
+  right: 35px;
+  font-size: 40px;
+  font-weight: bold;
+  transition: 0.3s;
+}
+
+.close:hover,
+.close:focus {
+  color: #bbb;
+  text-decoration: none;
+  cursor: pointer;
 }
 </style>
